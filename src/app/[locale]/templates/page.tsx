f905012a3f3165c7e2ea70bd72dfd01cd7f4b61a
fsuels <suelsferro@hotmail.com>
2025-05-20 10:31:07 -0400

    feat: add browse templates page

---
+++ src/app/[locale]/templates/page.tsx
@@ -0,0 +1,9 @@
+import TemplatesClientContent from './templates-client-content';
+import { generateStaticParams } from '../page';
+
+export { generateStaticParams };
+
+export default function TemplatesPage({ params }: { params: { locale: 'en' | 'es' } }) {
+  const { locale } = params;
+  return <TemplatesClientContent locale={locale} />;
+}
